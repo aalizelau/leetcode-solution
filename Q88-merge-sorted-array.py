99fd7d6b7de6b67d5be78ff27dc2e1798d35a0b1
Aalizelau <aalizelau@gmail.com>
2024-09-28 21:10:15 +0800

    done

--- Q88-merge-sorted-array.py
+++ Q88-merge-sorted-array.py
@@ -1,9 +1,18 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
+def merge(nums1, m, nums2, n):
+    mergedpointer  = m+n-1
+    pointer1 = m-1
+    pointer2 = n-1
+
+    
+    while pointer2 >=0:
+        nums1[mergedpointer] = nums2[pointer2]
+        pointer2 -=1
+        mergedpointer -=1
+    while pointer1 and pointer2 >=0:
+        if nums1[pointer1] > nums2[pointer2]:
+            nums1[mergedpointer] = nums1[pointer1]
+            pointer1 -=1 
+        else:
+            nums1[mergedpointer] = nums2[pointer2]
+            pointer2 -=1
+        mergedpointer -=1
