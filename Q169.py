814729cabaa3ad87de019a0e25bf011d308154af
Aalizelau <aalizelau@gmail.com>
2024-10-01 23:55:56 +0800

    done

---
+++ Q169.py
@@ -0,0 +1,9 @@
+def majorityElement(nums):
+    counts = {}
+    majority_count = len(nums) // 2
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+
+        if counts[num] > majority_count:
+            return num
