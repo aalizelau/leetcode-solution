8ba5723d6cadd8f8db601af2c51555a89f303b22
Aalizelau <aalizelau@gmail.com>
2024-10-10 23:55:52 +0800

    done

---
+++ Q55.py
@@ -0,0 +1,7 @@
+def canJump(nums):
+    max_reachable = 0
+    for i in range(len(nums)):
+        if i > max_reachable:
+            return False 
+        max_reachable = max(max_reachable, i + nums[i])
+    return True
